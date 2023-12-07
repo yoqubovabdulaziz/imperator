da52ea90d6c30e0f8976ab627e7c104af7546b46
yoqubovabdulaziz <abdulazizdasturlash@gmail.com>
2023-12-07 22:32:23 +0500

    starting

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  content: ["pages/about.html"],
+  content: ["pages/basket.html"],
+  content: ["pages/category.html"],
+  content: ["pages/login.html"],
+  content: ["pages/partner.html"],
+  content: ["pages/payment.html"],
+  content: ["pages/product-1.html"],
+  content: ["pages/product-2.html"],
+  content: ["pages/product-3.html"],
+  content: ["pages/product-4.html"],
+  content: ["pages/profil.html"],
+  content: ["pages/registration-product-1.html"],
+  content: ["pages/registration-product-2.html"],
+  content: ["pages/registration.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
